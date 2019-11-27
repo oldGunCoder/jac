f0fad187fa813bd38c3748453eb1a8eaf81d93ea
penggao <concordia.ie@gmail.com>
2019-11-27 18:37:30 -0500

    Added 3 new tables.

---
+++ Class4DB/Class4DB/Class.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Class]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ClassName] VARCHAR(50) NOT NULL, 
+    [ClassNumber] INT NOT NULL
+)
---
+++ Class4DB/Class4DB/Student.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Student]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [LastName] VARCHAR(50) NOT NULL, 
+    [Age] INT NOT NULL
+)
