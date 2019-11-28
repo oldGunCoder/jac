3d6f89c6cc9fc3f9078522d864907ede1a2f07d1
penggao <concordia.ie@gmail.com>
2019-11-28 18:35:14 -0500

    add a function

---
+++ Class4DB/Class4DB/udf_GetAllStudentsPerClass.sql
@@ -0,0 +1,22 @@
+﻿CREATE FUNCTION [dbo].[udf_GetAllStudentsPerClass]
+(
+	@classId int
+)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @result int
+
+	IF EXISTS (SELECT 1 FROM Student)
+	BEGIN
+		SELECT COUNT(1) FROM Student_Class WHERE ClassId = @classId
+
+		IF (@result IS NULL)
+		BEGIN
+			SET @result = 0
+		END
+	END
+
+
+	RETURN @result
+END
