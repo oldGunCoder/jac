220d3969772e13057e6c222caa0e47c31e0f8626
oldGunCoder <concordia.ie@gmail.com>
2019-12-02 14:54:34 -0500

    Created  stored procedure to get student and class info by age

---
+++ Class4DB/Class4DB/sp_GetStudentAndClassInfoByAge.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[sp_GetStudentAndClassInfoByAge]
+	@StudentAge int
+AS
+BEGIN
+	SELECT *
+	FROM dbo.udtf_GetAllStudentsByAge(@StudentAge) fn
+	INNER JOIN Class
+	ON fn.Id = Class.Id
+END
+GO;
+
+EXEC dbo.sp_GetStudentAndClassInfoByAge @StudentAge = 22;
